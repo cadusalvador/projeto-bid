905f18296551db961726c64d75d881f74b539167
Carlos <carlos1992.ces@gmail.com>
2024-01-30 22:13:00 -0300

    primeiro commit da API

---
+++ api/bid/src/test/java/br/com/carlos/bid/BidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlos.bid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
